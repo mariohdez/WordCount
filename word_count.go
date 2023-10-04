08e391ad1aa5933708b41b70c6ab0a87464ca0fa
Mario Hernandez <mhmario963@gmail.com>
2023-10-03 23:46:29 -0400

    got a decent working example for word count if all characters are in ascii.

--- word_count.go
+++ word_count.go
@@ -13,7 +13,9 @@ func main() {
 
 	switch num_args := len(os.Args); num_args {
 	case 1:
+		break
 	case 2:
+		break
 	case 3:
 		if _, ok := known_flags[os.Args[1]]; !ok {
 			os.Exit(1)
@@ -30,23 +32,29 @@ func main() {
 		}
 
 		fp = file
+		break
 	default:
 		os.Exit(1)
+		break
 	}
 
-	total_bytes := readFile(fp, data_to_display)
+	total_bytes, total_lines, total_words := readFile(fp, data_to_display)
 
 
 	defer fp.Close()
 
-	fmt.Printf("\t%d\t%s\n", total_bytes, os.Args[2])
+	fmt.Printf("  %d   %d  %d %s\n", total_lines, total_words, total_bytes, os.Args[2])
 
 	os.Exit(0)
 }
 
-func readFile(fp *os.File, data_to_display string) int {
+func readFile(fp *os.File, data_to_display string) (int, int, int) {
 	total_bytes := 0
-	buffer := make([]byte, 64)
+	total_lines := 0
+	total_words := 0
+	buffer_size := 64
+
+	buffer := make([]byte, buffer_size)
 
 	for {
 		bytes_read, err := fp.Read(buffer)
@@ -59,8 +67,40 @@ func readFile(fp *os.File, data_to_display string) int {
 		}
 
 		total_bytes += bytes_read
+
+		for i:= 0; i < bytes_read; {
+			prev_location := i
+			for i < bytes_read && !isWhitespace(buffer[i]) {
+				i++
+			}
+
+
+			if prev_location != i {
+				total_words += 1
+			}
+
+			for i < bytes_read && isWhitespace(buffer[i]) {
+				if (buffer[i] == 10) {
+					total_lines += 1
+				}
+
+				i++
+			}
+		}
 	}
 
-	return total_bytes
+	return total_bytes, total_lines, total_words
+}
+
+func isWhitespace(b byte) bool {
+	if (b > 8 && b < 14) {
+		return true
+	}
+
+	if (b == 32) {
+		return true
+	}
+
+	return false
 }
 
